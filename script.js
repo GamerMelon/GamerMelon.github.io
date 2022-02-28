ac66895790fce73432db6fc53be85e939ed97402
Wolfe Bowman <thewatermelon@icloud.com>
2022-02-28 17:32:49 -0500

    adding javascript for buttons

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ function initButtons(){
         $('#project').hide();
         document.getElementById("#logs").style.gridColumn =  "4/6";
     });
-    $("#affiliationButton").on("click", function(){
-        $('table').hide();
-        showProjectTable();
+    $("#add_job").on("click", function(){
+        console.log("adding log!")
+    });
+    $("#minus_job").on("click", function(){
+        console.log("taking a job out!")        
     });
 }
+    $(document).ready(function(){
+        initButtons();
+    });
