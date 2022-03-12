d2bb46d9814c3627409a8ccc27cbe66d282c4748
GamerMelon <83375939+GamerMelon@users.noreply.github.com>
2022-03-12 13:29:14 -0500

    script to generate logs

--- log_data.php
+++ log_data.php
@@ -1,5 +1,5 @@
 
-    ([
+([
  {
    "Time": "12:45AM",
    "Log": "Watered about 1/2 cup of filtered water"
--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,34 @@ function initButtons(){
     $(document).ready(function(){
         initButtons();
     });
+
+    function populateLogs(){
+        $.ajax({
+            url:'log_data.php',
+            dataType:'jsonp',
+            success:(data) => {
+                //let str=[varStr];//so first option will be the default value, will be signal for no filter later
+                let str = "<ul>";
+                //let button = `<option value=${varStr}>${name}</option>`;//making the first option, not sure if varStr works here
+                for (let obj of data){
+                    let time = obj["Time"];//get the specific field from the object
+                    let log = obj["Log"];
+                    let concat = `<li>At: ${time}<br>${log}</li>`
+                    str.push(concat);
+                    //curr = curr.toUpperCase();//uppercase for no duplicates
+                    //if (!str.includes(curr) && curr != '') {//if the current value is not in the list, add it to the list
+                    //    str.push(curr);
+                    //}
+
+                }
+                //str.sort();//sorting for peace of mind
+                //for (let i = 0; i < str.length; i++){//make the other options
+                //    button += `<option value=${str[i]}>${str[i]}</option>`;
+                //}
+                str.push("</ul>");
+                $(`#log_dump`).html(str);
+                //$(`#${place}`).html(button);//add the options to the specified select box
+            }
+        });
+    };
+    populateLogs();
